b92e05a0d47ee73d0e64544fbd928dd669bd703b
Adarsh Pannu <13154869+adarshpannu@users.noreply.github.com>
2021-01-14 21:49:56 -0800

    WIP

--- src/main.rs
+++ src/main.rs
@@ -22,3 +22,35 @@ fn slowest_key(keytimes: &[(i32, i32)]) -> i32 {
     dbg!(&max);
     0
 }
+
+/*
+public void recordTweet(String tweetName, long time);
+
+public long[] getTweetCountsPerFrequency(Frequency freq, String tweetName, long startTime, long endTime);
+
+enum Frequency {
+	MINUTE,
+	HOUR,
+	DAY
+}
+*/
+
+enum EventType {
+    TWEET,
+    RETWEET,
+    LIKE
+}
+
+enum Granularity {
+	MINUTE,
+	HOUR,
+	DAY
+}
+
+fn record_tweet(event_type: EventType, timestamp: u64) {
+
+}
+
+fn getEventCount(event_type: EventType, start: u64, end: u64, granularity: Granularity) {
+
+}
