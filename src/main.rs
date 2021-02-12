c410e70663c66eaebbfc400abc7968e46d6b9bae
Adarsh Pannu <13154869+adarshpannu@users.noreply.github.com>
2021-02-11 21:52:55 -0800

    More lifetime experiments

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,34 @@
 #![allow(warnings)]
+use std::collections::HashMap;
+use std::str;
 
-mod so41270052;
-
-struct Person<'a> {
-    full_name: String,
-    first_name: &'a str,
+#[derive(Debug)]
+pub enum ParserError {
+    Generic
 }
 
-impl<'a> Person<'a> {
-    fn set_name(&mut self) {
-        self.first_name = &self.full_name[..4];
+pub struct Resource(
+    pub HashMap<String, String>
+);
+
+pub struct Parser<'a> {
+    source: Option<str::Chars<'a>>
+}
+
+impl<'a> Parser<'a> {
+    pub fn new() -> Parser<'a> {
+        Parser { source: None }
+    }
+    pub fn parse(&mut self, source: &'a str) -> Result<Resource, ParserError> {
+        self.source = Some(source.chars());
+
+        let entries = HashMap::new();
+        Ok(Resource(entries))
     }
 }
 
-fn main() {}
+fn main() {
+    let mut parser = Parser::new();
+    parser.parse("key1 = Value 1");
+    parser.parse("key2 = Value 2");
+}
