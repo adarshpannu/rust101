c9ce11cf85681a120170b7fd6b0ec332796f35ea
Adarsh Pannu <13154869+adarshpannu@users.noreply.github.com>
2021-03-11 12:03:53 -0800

    Playing with threads

--- src/threads.rs
+++ src/threads.rs
@@ -1,7 +1,24 @@
+#![allow(warnings)]
+use std::{thread, time};
 
+fn do_loop(s: &str, limit: i32) {
+    let ten_millis = time::Duration::from_millis(100);
 
+    for i in 1..=limit {
+        println!("{}: {}", s, i);
+        thread::sleep(ten_millis);
+    }
+}
 
 #[test]
 fn test() {
-    println!("Hello")
+    let handle = thread::spawn(|| {
+        do_loop("child", 10)
+    });
+
+    handle.join().unwrap();
+
+    do_loop("parent", 5);
+
 }
+
