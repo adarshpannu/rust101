a6ba0f611cf05eb982c6ecba8c913aba14c80af5
Adarsh Pannu <13154869+adarshpannu@users.noreply.github.com>
2021-03-12 14:24:50 -0800

    WIP

---
+++ src/mutexes.rs
@@ -0,0 +1,17 @@
+#![allow(warnings)]
+
+use std::sync::Mutex;
+use std::thread;
+use std::sync::Arc;
+
+#[test]
+fn test() {
+    let mtx = Arc::new(Mutex::new(10));
+
+    for i in 0..5 {
+        let mtx2 = Arc::clone(&mtx);
+        let handle = thread::spawn(move || {
+            let val = mtx2.lock().unwrap();
+        });
+    }
+}
